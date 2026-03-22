37d35709dc8cf02e7292cde808529a62aae1d63d
Derek-ma-bxsci <derekm105@nycstudents.net>
2026-03-22 18:49:55 +0000

    works

--- SudokuBoard.java
+++ SudokuBoard.java
@@ -11,39 +11,46 @@ public class SudokuBoard{
         return board;
     }
 
-    // REMINDER THAT METHODS CANT ACTUALLY CHANGE VARIABLES IMMEDIATELY YOU HAVE TO MAKE ANOTHER THEN SET IT TO THAT ONE
-    public void initializeBoard(int[][] board){
-        for (int i = 0; i < board.length; i++){
-            for (int j = 0; j < board[0].length; j++){
-                board[i][j] = 0;
-            }
-        }
-    }
-
     public int[][] createValidBoard(){
-        int[][] board = new int[9][9];
-        // first fill all numbers with 0
-        for (int i = 0; i < board.length; i++){
-            for (int j = 0; j < board[0].length; j++){
-                board[i][j] = 0;
-            }
-        }
+        int[][] newBoard = new int[9][9];
 
         // keep filling board with random nums that are not already used
-        while (findNextEmpty(board)[0] != -1){
+        while (findNextEmpty(newBoard)[0] != -1){
             boolean availableValuePut = false;
-            ArrayList<Integer> usedNums = new ArrayList<>();
             while (!availableValuePut){
-                int num = randomDistinct9(usedNums);
-                int[] coords = findNextEmpty(board);
-                if (!binaryContains(board, num, coords[0], coords[1])){
-                    board[coords[0]][coords[1]] = num;
-                    availableValuePut = true;
+                int[] coords = findNextEmpty(newBoard);
+                ArrayList<Integer> shuffledNums = randomNumList();
+                for (int i = 0; i < shuffledNums.size() && !availableValuePut; i++){
+                    int num = shuffledNums.get(i);
+                    if (isValid(newBoard, num, coords[0], coords[1])){
+                        newBoard[coords[0]][coords[1]] = num;
+                        availableValuePut = true;
+                    }
                 }
-                usedNums.add(num);
+                if (!availableValuePut){
+                    return createValidBoard();
+                }
             }
         }
-        return board;
+        return newBoard;
+    }
+    
+    public ArrayList<Integer> randomNumList(){
+        ArrayList<Integer> list = new ArrayList<>(); 
+        for (int i = 1; i <= 9; i++){
+            list.add(i);
+        }
+        list = shuffle(list);
+        return list;
+    }
+    public ArrayList<Integer> shuffle(ArrayList<Integer> list){
+        for (int i = 0; i < 50; i++){
+            int index = (int)(Math.random() * 9);
+            int num = list.get(index);
+            list.remove(index);
+            list.add(num);
+        }
+        return list;
     }
     public int[] findNextEmpty(int[][] board){
         for (int i = 0; i < board.length; i++){
@@ -58,51 +65,24 @@ public class SudokuBoard{
         return coords;
     }
 
-    public int randomDistinct9(ArrayList<Integer> usedNums){
-        int randomNum = (int)(Math.random() * 9) + 1;
-        while (usedNums.contains(randomNum)){
-            randomNum = (int)(Math.random() * 9) + 1;
-        }
-        return randomNum;
-    }
-    public boolean binaryContains(int[][] a, int num, int rowIndex, int colIndex){
-        // finds values in row
-        int[] row = a[rowIndex];
-
-        // finds values in column
-        int[] col = new int[9];
-        for (int i = 0; i < a.length; i++){
-            col[i] = a[i][colIndex];
-        }
-
-        // finds values in square
-        int[] square = new int[9];
-        int startRowIndex = (rowIndex / 3) * 3;
-        int startColIndex = (colIndex / 3) * 3;
-        int regIndex = 0; 
-        for (int i = startRowIndex; i < startRowIndex + 3; i++){
-            for (int j = startColIndex; j < startColIndex + 3; j++){
-                square[regIndex] = a[i][j];
-                regIndex++;
+    
+    public boolean isValid(int[][] board, int num, int row, int col) {
+        // Check row and column
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == num || board[i][col] == num) {
+                return false;
             }
         }
-
-        return (binaryIsIn(row, num) || binaryIsIn(col, num) || binaryIsIn(square, num));
-    }
-    
-    public boolean binaryIsIn(int[] array, int num){
-        Arrays.sort(array);
-        int left = 0;
-        int right = array.length - 1;
-        while (left <= right) {
-            int middle = (left + right) / 2;
-            if (num == array [middle])
-                return true;
-            else if (num < array[middle])
-                right = middle - 1;
-            else if (num > array[middle])
-                left = middle + 1;
+        // Check box
+        int startRow = row - row % 3;
+        int startCol = col - col % 3;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i + startRow][j + startCol] == num) {
+                    return false;
+                }
+            }
         }
-        return false;
+        return true;
     }
 }
